e861ea1dc9903a58a1509de23a86b797b3b7ed4c
seirin <43994152+yumseirin@users.noreply.github.com>
2021-05-27 18:08:30 +0800

    数据类型的代码

---
+++ Python/Python3.9/day02/数据类型.py
@@ -0,0 +1,41 @@
+# 整型int
+print(666)
+
+print(2 + 10)
+
+print(2 * 10)
+
+print(10 / 2)
+
+print(10 % 3)
+
+print(2 ** 4)
+
+# 字符串str
+print("我是seirin")
+print('我是seirin')
+print('我是"seirin')
+
+print("""这里
+可以
+放
+多行""")
+print('''这里
+可以放多行''')
+
+print("seirin" + "在学习Python")
+print(3 * "我好饿！")
+
+# 布尔bool
+print(1 > 2)
+print(False)
+
+print(1 == 1)
+print(True)
+
+name = input("请输入你的用户名:")
+
+if name == "seirin":
+    print("用户登录成功")
+else:
+    print("用户登录失败")
